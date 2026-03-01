d1f381356d462c17110f0e9286f843ca3cd2e323
lolabdelwahed-ctrl <lolabdelwahed@gmail.com>
2026-02-28 20:38:47 -0500

    function

--- final_grade_calc.py
+++ final_grade_calc.py
@@ -6,18 +6,15 @@ current = 0
 target = 0
 final_weight = 0
 
-current = float(input('Your current grade is (numbers only please): '))
-target = float(input('You want at least (numbers only please): '))
-final_weight = float(input('Your final is worth (numbers only please): '))
+def fin_grade(current,target,final_weight):
+  user_final_weight = user_final_weight/100
+  final_score_req = (target-(current*(1-final_weight)))/final_weight
+  return final_score_req
 
-print (current)
-print (target)
-print (final_weight)
-# to check if everything ran smoothly
-final_weight = final_weight/100
+user_current = float(input('Your current grade is (numbers only please): '))
+user_target = float(input('You want at least (numbers only please): '))
+user_final_weight = float(input('Your final is worth (numbers only please): '))
 
-
-final_score_req = (target-(current*(1-final_weight)))/final_weight
-
-print (f'you need at least a {final_score_req:.2f} in order to achieve your target grade')
+You_ok = fin_grade(user_current,user_target,user_final_weight)
+print (f'you need at least a {You_ok:.2f} in order to achieve your target grade')
 
